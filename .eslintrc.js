fcb46431a8e6c292e8e2b7c68a6a6329c9f7c5ed
Kelvin Lee <kelvin.rodriguez@nurx.co>
2020-02-23 16:19:47 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    // parser: '@typescript-eslint/parser',
+    // project: './tsconfig.json',
+    // extraFileExtensions: ['.vue'],
+  },
+  extends: [
+    'prettier/vue',
+    'plugin:nuxt/recommended',
+    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'only-multiline' ],
+    'vue/html-self-closing': ['error'],
+    'vue/multiline-html-element-content-newline': ['error'],
+    'vue/valid-template-root': ['error'],
+  }
+}
---
+++ layouts/error.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+interface ErrorData {
+  pageNotFound: string;
+  otherError: string;
+}
+
+interface HeadData {
+  title: string;
+}
+
+export default Vue.extend({
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data (): ErrorData {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    };
+  },
+  head (): HeadData {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
+    return {
+      title
+    };
+  }
+});
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
