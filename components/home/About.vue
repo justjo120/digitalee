d58ec5aae70944cf30b349609a7f7862f49314a5
justjo120 <johannarklay@gmail.com>
2020-02-27 01:02:33 -0600

    Start About.vue

---
+++ components/home/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-layout class="d-flex justify-center align-content-center">
+        <v-flex xs10>
+            <div class="tile">
+                About
+            </div>
+            <div class="body-2">
+                DigitaLee aims to bring quality service to help your business navigate through the ever changing digital world.  
+            </div>
+        </v-flex>
+    </v-layout>
+</template>
+<script>
+</script>    
+    
